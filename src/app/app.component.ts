c3371c4a36f204158e26821aaf0b19507a007241
FabianJ <fabianjacho@gmail.com>
2025-07-04 12:58:46 -0500

    ejercicio con componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { ListaComponent } from './lista/lista.component'; // Asegúrate de importar el componente hijo
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule, CommonModule], // <-- Agrega FormsModule aquí
+  imports: [RouterOutlet, FormsModule, CommonModule,ListaComponent], // <-- Agrega FormsModule aquí
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 
 export class AppComponent {
-  title = 'TodoList';
+  title = 'TodoList - Ejercicio de Lista con componentes🎉';
 
   item: string = '';
   lista1:{id: number, nombre: string}[] = [];
@@ -24,6 +25,14 @@ export class AppComponent {
 
   remover(index: number) {
     this.lista1 = this.lista1.filter(item => item.id !== index);
+  }
+
+  recibirMensaje(mensaje: string){
+    console.log('Recibido desde hijo: ',mensaje );
+  }
+
+  trackById(index: number, item: {id: number, nombre: string}) {
+    return item.id;
   }
 
 }
--- src/app/lista/lista.component.ts
+++ src/app/lista/lista.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
+//componente hijo
 @Component({
-  selector: 'app-lista',
+  selector: 'app-lista',    
   imports: [],
   templateUrl: './lista.component.html',
   styleUrl: './lista.component.scss'
 })
+
 export class ListaComponent {
+  @Input() item!: string;   //se llena desdde el padre
+
+  @Output() mensaje = new EventEmitter<string>(); //lo que se envia al padre
+
+  enviarAlPadre(){
+    this.mensaje.emit('Dato recibido: ' + this.item);
+  }
+
+
   lista1:{id: number, nombre: string}[] = [];
 
   remover(index: number) {
